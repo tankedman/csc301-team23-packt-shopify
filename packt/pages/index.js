39a07d29171fd0089122e41750fc8fa64b071524
tankedman <jedwinmok@gmail.com>
2021-10-25 17:01:48 -0400

    initial ui functionality

--- packt/pages/index.js
+++ packt/pages/index.js
@@ -1,9 +1,25 @@
-import { Heading, Page } from "@shopify/polaris";
+import { Heading, Page, TextField, FormLayout, Button } from "@shopify/polaris";
 
 const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
+  <Page
+    title = 'Packt'
+    primaryAction={{
+      content: 'select products',
+      onAction: () => console.log('clicked')
+    }}>
+
+    <FormLayout>
+      <TextField label="Phone Number" onChange={() => {}} autoComplete="off" />
+      <TextField
+        type="email"
+        label="Number of Bags"
+        onChange={() => {}}
+        autoComplete="email"
+      />
+      <Button submit>Submit</Button>
+    </FormLayout>
   </Page>
 );
+
 
 export default Index;
