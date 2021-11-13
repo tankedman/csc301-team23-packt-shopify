e24708a358691ddcb9e77c721d5b91d4f5f50d27
tankedman <jedwinmok@gmail.com>
2021-11-12 21:39:29 -0500

    api login

--- packt/packt_api/call_packt.js
+++ packt/packt_api/call_packt.js
@@ -5,13 +5,22 @@ fetch('https://packt-project-development.herokuapp.com/login')
   .then(data => console.log(data))
   .catch(err => console.log(err));
 
+let todo = {
+  phoneNumber: 9054164444,
+  password: "admin"
+};
 
-
-fetch('https://packt-project-development.herokuapp.com/users', {
+fetch('https://packt-project-development.herokuapp.com/login', {
   method: 'POST',
-  body: JSON.stringify(),
-  headers: {}
+  body: JSON.stringify(todo),
+  headers: { 'Content-Type': 'application/json' }
 })
+  .then(response => response.text())
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+
+fetch('https://packt-project-development.herokuapp.com/users')
   .then(response => response.text())
   .then(data => console.log(data))
   .catch(err => console.log(err));
