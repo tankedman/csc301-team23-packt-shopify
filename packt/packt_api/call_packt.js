556d9bebd8b7c1204c84e3d491f126f4ed48eadf
tankedman <jedwinmok@gmail.com>
2021-11-12 21:35:30 -0500

    api post

--- packt/packt_api/call_packt.js
+++ packt/packt_api/call_packt.js
@@ -1,7 +1,17 @@
 const fetch = require('node-fetch')
 
-fetch('https://packt-project-development.herokuapp.com/users')
+fetch('https://packt-project-development.herokuapp.com/login')
   .then(response => response.text())
   .then(data => console.log(data))
   .catch(err => console.log(err));
 
+
+
+fetch('https://packt-project-development.herokuapp.com/users', {
+  method: 'POST',
+  body: JSON.stringify(),
+  headers: {}
+})
+  .then(response => response.text())
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
