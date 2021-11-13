2d70462f788e86ef2c00dad7dee481c53b3c6169
tankedman <jedwinmok@gmail.com>
2021-11-12 21:31:10 -0500

    api

--- packt/packt_api/call_packt.js
+++ packt/packt_api/call_packt.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch')
 
-const apiResponse = await fetch('https://packt-project-development.herokuapp.com/users')
-  .then(response => response.json())
+const apiResponse = await fetch('https://google.com')
+  .then(response => response.text())
   .then(data => console.log(data))
+  .catch(err => console.log(err));
 
--- packt/server/index.js
+++ packt/server/index.js
@@ -3,5 +3,12 @@ require('@babel/register')({
   ignore: ['node_modules']
 });
 
+
 // Import the rest of our application.
 module.exports = require('./server.js');
+
+const fetch = require('node-fetch')
+
+const apiResponse = await fetch(
+  'https://cat-fact.herokuapp.com/facts/' 
+)
