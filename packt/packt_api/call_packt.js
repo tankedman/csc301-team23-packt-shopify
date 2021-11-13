5fdccd279c55520b036634ade827011ed4dc8b63
tankedman <jedwinmok@gmail.com>
2021-11-12 21:33:18 -0500

    api get working

--- packt/packt_api/call_packt.js
+++ packt/packt_api/call_packt.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 
-const apiResponse = await fetch('https://google.com')
+fetch('https://packt-project-development.herokuapp.com/users')
   .then(response => response.text())
   .then(data => console.log(data))
   .catch(err => console.log(err));
